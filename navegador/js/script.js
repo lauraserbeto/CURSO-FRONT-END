43c1558302c9d9bc81b6117f8bf0d6e873c41c9c
Laura Serbêto <115957780+lauraserbeto@users.noreply.github.com>
2025-06-16 14:53:19 -0300

    Delete navegador directory

--- navegador/js/script.js
+++
@@ -1,5 +0,0 @@
-//1 - movendo-se pelo DOM
-console.log(document.body);
-console.log(document.body.childNodes);
-console.log(document.body.childNodes[1].childNodes); //header
-console.log(document.body.childNodes[1].childNodes[1].textContent); //Título

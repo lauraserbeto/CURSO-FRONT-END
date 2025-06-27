45c381b596eb34290c47f4214c7eb1af83ec8819
Laura Serbêto <115957780+lauraserbeto@users.noreply.github.com>
2025-06-27 09:09:12 -0300

    Add files via upload

---
+++ axios/js/script.js
@@ -0,0 +1,19 @@
+//1- instalação
+console.log(axios);
+
+//2 - meu primeiro request
+const getData = async() =>{
+    try{
+        const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+        );
+
+        console.log(response);
+
+        return response.data;
+    } catch (error){
+        console.log(error);
+    }
+};
+
+getData();
